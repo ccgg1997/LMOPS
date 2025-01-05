bb9befb22418889550f830ac6df48d0c4223969f
brigittechavez <brigitte.chavez@correounivalle.edu.co>
2025-01-04 23:36:02 -0500

    S3 client and downloading data from S3

--- main.py
+++ main.py
@@ -2,47 +2,76 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pickle
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pickle
+import boto3
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+from datetime import datetime
 
-Data = pd.read_csv('kc_house_data.csv')
-Data.head(5)
 
-Data = Data.drop('date',axis=1)
-Data = Data.drop('id',axis=1)
-Data = Data.drop('zipcode',axis=1)
+
+
+s3_client = boto3.client('s3') #CLIENTE DE S3
+
+
+bucket_name = 'datamlops'
+file_key = 'datahouse.csv'
+local_file_path = 'kc_house_data.csv'
+
+
+s3_client.download_file(bucket_name, file_key, local_file_path)#DESCARGA DESDE S3
+print(f"Archivo descargado desde S3: {file_key}")
+
+
+
+Data = pd.read_csv(local_file_path)#LEER LOS DATOS
+print("primeras filas del DataFrame:")
+print(Data.head(5))
+
+Data = Data.drop(['date', 'id', 'zipcode'], axis=1, errors='ignore')
+
 
 X = Data.drop('price',axis =1).values
 y = Data['price'].values
 
+
+print("Etiquetas (y):")
 print(y)
 
-#splitting Train and Test
-from sklearn.model_selection import train_test_split
+#DIVIDIR LOS DATOS EN CONJUNTOS DE ENTRENAMIENTO Y PRUEBA
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=101)
 
 
 # Multiple Liner Regression
-from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
 #predicting the test set result
 y_pred = regressor.predict(X_test)
 
-# # visualizing residuals
-# fig = plt.figure(figsize=(10,5))
-# residuals = (y_test- y_pred)
-# # sns.distplot(residuals)
+#VISUALIZAR DATOS RESUDIALES
+#fig = plt.figure(figsize=(10,5))
+#residuals = (y_test- y_pred)
+#fig = plt.figure(figsize=(10, 5))
+#sns.histplot(residuals, kde=True, color='blue', bins=30)
+#plt.title("Distribución de los Residuales")
+#plt.xlabel("Residual")
+#plt.ylabel("Frecuencia")
+#plt.show()
+
 
 #compare actual output values with predicted values
-y_pred = regressor.predict(X_test)
 df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
 df1 = df.head(10)
 print(df1)
 
 
-# # evaluate the performance of the algorithm (MAE - MSE - RMSE)
-# from sklearn import metrics
-# print('MAE:', metrics.mean_absolute_error(y_test, y_pred))
-# print('MSE:', metrics.mean_squared_error(y_test, y_pred))
-# print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
-# print('VarScore:',metrics.explained_variance_score(y_test,y_pred))
+#EVALUACIÓN DEL RENDIMIENTO
+print('MAE:', metrics.mean_absolute_error(y_test, y_pred))
+print('MSE:', metrics.mean_squared_error(y_test, y_pred))
+print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+print('VarScore:', metrics.explained_variance_score(y_test, y_pred))
